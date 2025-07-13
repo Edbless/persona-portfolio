54c74b9242321aed7b04d61778dceb982f4ba884
Edbless <giftyhemans8@gmail.com>
2025-07-13 10:14:31 +0100

    personal portfolio commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Skills } from './pages/skills/skills';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home }, // Default route
+  { path: 'about', component: About },
+  { path: 'skills', component: Skills },
+  { path: 'contact', component: Contact }
+];
